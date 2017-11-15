e1a49059bc39dfcbd202d8b71c124d3327787646
Unknown <james.smith@heig-vd.ch>
2017-11-15 13:39:25 +0100

    Java Doc pour le package hanoi et main

--- Labo7_Hanoi/src/hanoi/HanoiDisplayer.java
+++ Labo7_Hanoi/src/hanoi/HanoiDisplayer.java
@@ -1,16 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hanoi;
 
 /**
- *
- * @author James
+ * Classe représentant l'affichage du jeu d'hanoi
+ * @author Adrien Alleman et James Smith
  */
 public class HanoiDisplayer {
     
+    /**
+     * Fonction affichange l'état du jeu à un certain tour
+     * @param h jeu d'hanoi qui est entrain d'etre jouer.
+     */
     public void display(Hanoi h){
         System.out.println("-- Turn: " + h.turn());
         System.out.println(h);
